9908cbc5a28b2128d9188b613c1046e98f20419e
Luuk van Oijen <lazyluuk.channel@gmail.com>
2022-08-22 23:40:46 +0200

    repo config?

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use std::collections::HashMap;
 use std::process::Command;
 use serde::Deserialize;
 
 mod packages_x86_64_file;
+mod pacman_conf_file;
 
 #[derive(Deserialize)]
 struct Config {
     packages: PackageList,
+    repos: HashMap<String, HashMap<String, String>>,
 }
 
 #[derive(Deserialize)]
@@ -25,6 +28,7 @@ fn main() {
         .subcommand(Command::new("clean").about("Cleans up package cache"))
         .subcommand(Command::new("build").about("Builds the repo. Warning: might take long!"))
         .subcommand(Command::new("update").about("Pulls any new iris-minimal repo changes"))
+        .subcommand(Command::new("config").about("Only generates and copies over configs"))
         .get_matches();
 
     match matches.subcommand() {
@@ -32,6 +36,7 @@ fn main() {
         Some(("clean", _)) => clean(),
         Some(("update", _)) => update(),
         Some(("build", _)) => build(),
+        Some(("config", _)) => config(),
         _ => panic!("How did we get here?")
     }
 }
@@ -83,6 +88,8 @@ fn build() {
 }
 
 fn config() {
+    use pacman_conf_file::Repo;
+
     println!("Building project...");
     println!("Reading config file...");
     let file_str = std::fs::read_to_string("config.toml").expect("Failed to open config file!");
@@ -91,4 +98,18 @@ fn config() {
     println!("Packages to be installed: {}", config.packages.packages.len());
     let packages_file_str = packages_x86_64_file::build(&config.packages.packages);
     std::fs::write("iris-minimal/archiso/packages.x86_64", packages_file_str).expect("Failed to overwrite packages.x86_64!");
+
+    println!("Repos to be used: {}", config.repos.len());
+    let mut repos = Vec::new();
+    for (name, value) in config.repos {
+        repos.push(Repo {
+            name: name,
+            sig_level: value.get("SigLevel").expect("SigLevel missing!").clone(),
+            server: value.get("Server").expect("Server missing!").clone(),
+        })
+    }
+    let pacman_conf_str = pacman_conf_file::build(&repos);
+    std::fs::write("iris-minimal/archiso/pacman.conf", pacman_conf_str).expect("Failed to overwrite pacman.conf!");
+
+    println!("Configuration handled!");
 }
